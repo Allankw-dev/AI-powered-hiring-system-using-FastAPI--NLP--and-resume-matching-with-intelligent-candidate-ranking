0d8eb7f7bd616584256bd45353034343f5bd9100
Allankw-dev <allankamau517@gmail.com>
2026-06-15 17:55:35 +0300

    Switch to PostgreSQL on Render

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -2,14 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from app.core.config import settings
 
-DATABASE_URL = (
-    f"mssql+pyodbc://{settings.SQL_SERVER}/{settings.SQL_DATABASE}"
-    f"?driver={settings.SQL_DRIVER.replace(' ', '+')}"
-    f"&trusted_connection=yes"
-)
-
 engine = create_engine(
-    DATABASE_URL,
+    settings.DATABASE_URL,
     echo=True,
     future=True
 )
